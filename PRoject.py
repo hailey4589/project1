48c5b4f23030370deb33a0e23a164844d6285610
hailey3456 <119626099+hailey3456@users.noreply.github.com>
2023-10-10 10:13:17 -0400

    working models
    yeah all three models work!

--- PRoject.py
+++ PRoject.py
@@ -1,3 +1,5 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 import pandas as pd
 # from pandas.plotting import scatter_matrix
 import numpy as np
@@ -5,14 +7,21 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.metrics import mean_squared_error, f1_score, precision_score, confusion_matrix
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import GridSearchCV
+from sklearn.linear_model import SGDClassifier
+from sklearn.naive_bayes import GaussianNB
 
 #import data
 df = pd.read_csv("Project_1_Data.csv")
 
 # print(df.isna().any(axis=0).sum())  #how many columns have missing values?
-#print(df.isna().any(axis=1).sum()) #how many rows have missing values?
+# print(df.isna().any(axis=1).sum()) #how many rows have missing values?
 #df = df.dropna()
-#df = df.reset_index(drop=True) 
+#df = df.reset_index(drop=True)
 
 print("First few rows of the DataFrame:")
 print(df.head())
@@ -24,12 +33,13 @@ print(df.info())
 print('\nDecribing data')
 print(df.describe())
 
-df.hist(bins=50, figsize=(20,15))
+df.hist(bins=50, figsize=(20, 15))
 plt.show()
-print(df.isna().any(axis=0).sum())  #how many columns have missing values?
-print(df.isna().any(axis=1).sum()) #how ma
+print(df.isna().any(axis=0).sum())  # how many columns have missing values?
+print(df.isna().any(axis=1).sum())  # how ma
 
-df["attributes"] = pd.cut(df["Z"],bins = [0,1,2,3,np.inf], right = False, labels= [0.5,1.5,2.5,3.5])
+df["attributes"] = pd.cut(
+    df["Z"], bins=[0, 1, 2, 3, np.inf], right=False, labels=[0.5, 1.5, 2.5, 3.5])
 df['attributes'].hist()
 
 nan_rows = df['attributes'].isna().any()
@@ -40,168 +50,184 @@ for train_index, test_index in split.split(df, df["attributes"]):
     strat_train_set = df.loc[train_index]
     strat_test_set = df.loc[test_index]
 
-corr_matrix = strat_train_set.corr(numeric_only=True)
-plt.figure(figsize=(10,8))
+df = strat_train_set.drop('Step', axis=1)
+df_labels = strat_train_set['Step'].copy()
+
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.scatter(df['X'], df['Y'], df['Z'])
+plt.show()
+
+
+df.hist(bins=50, figsize=(20, 15))
+plt.show()
+
+
+corr_matrix = df.corr(numeric_only=True)
+plt.figure(figsize=(10, 8))
 plt.matshow(corr_matrix)
-sns.heatmap(np.abs(corr_matrix));
-
-
-
-#anything from here, unless for testing, should be on only the train dataset
-#to avoid data snooping bias.
-
-
-
-train_y = strat_train_set['median_house_value']
-df_X = strat_train_set.drop(columns = ["median_house_value"])
-
-my_scaler = StandardScaler()
-my_scaler.fit(df_X.iloc[:,0:-5])
-scaled_data = my_scaler.transform(df_X.iloc[:,0:-5])
-scaled_data_df = pd.DataFrame(scaled_data, columns=df_X.columns[0:-5])
-train_X = scaled_data_df.join(df_X.iloc[:,-5:])
-
-# train_X["rooms_per_household"] = train_X["total_rooms"]/train_X["households"]
-# train_X["bedrooms_per_room"] = train_X["total_bedrooms"]/train_X["total_rooms"]
-# train_X["population_per_household"]=train_X["population"]/train_X["households"]
-
-columns_list = train_X.columns.tolist()
-new_order = ['longitude',
-  'latitude',
-  'housing_median_age',
-  'total_rooms',
-  'total_bedrooms',
-  'population',
-  'households',
-  'median_income',
-  # 'rooms_per_household',
-  # 'bedrooms_per_room',
-  # 'population_per_household',
-  'ocean_proximity_<1H OCEAN',
-  'ocean_proximity_INLAND',
-  'ocean_proximity_ISLAND',
-  'ocean_proximity_NEAR BAY',
-  'ocean_proximity_NEAR OCEAN']
-train_X = train_X[new_order]
-
-corr_matrix = (train_X.iloc[:,0:-5]).corr()
-sns.heatmap(np.abs(corr_matrix))
-corr1 = np.corrcoef(train_X['longitude'], train_y)
-print("longitude correlation wiht y is: ", corr1[0,1])
-corr2 = np.corrcoef(train_X['latitude'], train_y)
-print("latitude correlation wiht y is: ",corr2[0,1])
-corr3 = np.corrcoef(train_X['total_rooms'], train_y)
-print("total rooms correlation wiht y is: ",corr3[0,1])
-corr4 = np.corrcoef(train_X['total_bedrooms'], train_y)
-print("total bedrooms correlation wiht y is: ",corr4[0,1])
-corr5 = np.corrcoef(train_X['population'], train_y)
-print("population correlation wiht y is: ", corr5[0,1])
-corr6 = np.corrcoef(train_X['households'], train_y)
-print("households correlation wiht y is: ", corr6[0,1])
-train_X = train_X.drop(['longitude'], axis=1)
-train_X = train_X.drop(['total_bedrooms'], axis=1)
-train_X = train_X.drop(['population'], axis=1)
-train_X = train_X.drop(['households'], axis=1)
-
-plt.figure()
-corr_matrix = train_X.corr()
 sns.heatmap(np.abs(corr_matrix))
 
-from sklearn.linear_model import LinearRegression
-model1 = LinearRegression()
-model1.fit(train_X, train_y)
+'''
+scaler = MinMaxScaler()
+df['X'] = scaler.fit_transform(df['X'],[0,1])
+print('norm df')
+print(df['X'])
 
-some_data = train_X.iloc[:10]
-some_data.columns = train_X.columns
-some_house_values = train_y.iloc[:10]
+'''
+# SGD classifier
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(df,df_labels)
 
-# for i in range(10):
-#     some_predictions = model1.predict(some_data.iloc[i].values.reshape(1, -1))
-#     some_actual_values = some_house_values.iloc[i]
-#     print("Predictions:", some_predictions)
-#     print("Actual values:", some_actual_values)
+sgd_pred = sgd_clf.predict(df)
+scores = cross_val_score(sgd_clf, df, df_labels,scoring="neg_mean_squared_error", cv=5)
+clf_scores = np.sqrt(-scores)
+print('scores')
+print(scores)
+print(scores.mean())
+print(scores.std())
+print('accuracy')
+acc = cross_val_score(sgd_clf, df, df_labels, cv=10,scoring="accuracy")
+print(acc)
 
-# model1_prediction = model1.predict(train_X)
-from sklearn.metrics import mean_absolute_error
-# model1_train_mae = mean_absolute_error(model1_prediction, train_y)
-# print("Model 1 training MAE is: ", round(model1_train_mae,2))
-
-
-from sklearn.ensemble import RandomForestRegressor
-model2 = RandomForestRegressor(n_estimators=30, random_state=42)
-model2.fit(train_X, train_y)
-model2_predictions = model2.predict(train_X)
-model2_train_mae = mean_absolute_error(model2_predictions, train_y)
-print("Model 2 training MAE is: ", round(model2_train_mae,2))
-
-
-# for i in range(10):
-#     some_predictions1 = model1.predict(some_data.iloc[i].values.reshape(1, -1))
-#     some_predictions2 = model2.predict(some_data.iloc[i].values.reshape(1, -1))
-#     some_actual_values = some_house_values.iloc[i]
-    # print("Predictions Model 1:", some_predictions1)
-    # print("Predictions Model 2:", some_predictions2)
-    # print("Actual values:", some_actual_values)
+param_grid = {'loss':['log_loss','modified_huber'],'penalty':['elasticnet'],'alpha':np.logspace(0, 10, 10),'l1_ratio':[0.05,0.06,0.07,0.08,0.09,0.1,0.12,0.13,0.14,0.15,0.2]}
+grid_search = GridSearchCV(sgd_clf, param_grid, cv=5,scoring='accuracy',return_train_score=True)
+sgd_clf = grid_search.fit(df, df_labels)
+print(grid_search.best_params_)
+sgd_pred = sgd_clf.predict(df)
+scores = cross_val_score(sgd_clf, df, df_labels,scoring="neg_mean_squared_error", cv=5)
+#clf_scores = np.sqrt(-scores)
+print('scores')
+print(scores)
+print(scores.mean())
+print(scores.std())
 
 
+# precsion, confusion matix, accuracy, F1 score
+print('accuracy')
+acc = cross_val_score(sgd_clf, df, df_labels, cv=10,scoring="accuracy")
+print(acc)
 
-#cross validation
-# from sklearn.model_selection import cross_val_score
-# model1 = LinearRegression()
-# model2 = RandomForestRegressor(n_estimators=30, random_state=42)
+dfpred = cross_val_predict(sgd_clf, df, df_labels, cv=5)
+#print('confusion matrix')
 
-# # Perform k-fold cross-validation for Model 1
-# scores_model1 = cross_val_score(model1, train_X, train_y, cv=5, scoring='neg_mean_absolute_error')
-# mae_model1 = -scores_model1.mean()
-# print("Model 1 Mean Absolute Error (CV):", round(mae_model1, 2))
+#print(confusion_matrix(df_labels, dfpred))
 
-# # Perform k-fold cross-validation for Model 2
-# scores_model2 = cross_val_score(model2, train_X, train_y, cv=5, scoring='neg_mean_absolute_error')
-# mae_model2 = -scores_model2.mean()
-# print("Model 2 Mean Absolute Error (CV):", round(mae_model2, 2))
+prec = precision_score(df_labels, sgd_pred, average = 'micro')
+print(prec)
 
+f1 = f1_score(df_labels, dfpred, average = 'micro')
+print(f1)
 
-
-
-#GridSearchCV
-# from sklearn.model_selection import GridSearchCV
-# param_grid = {
-#     'n_estimators': [10, 30, 50],
-#     'max_depth': [None, 10, 20, 30],
-#     'min_samples_split': [2, 5, 10],
-#     'min_samples_leaf': [1, 2, 4],
-#     'max_features': ['sqrt', 'log2']
-# }
-# model2 = RandomForestRegressor(random_state=42)
-# grid_search = GridSearchCV(model2, param_grid, cv=5, scoring='neg_mean_absolute_error', n_jobs=-1)
-# grid_search.fit(train_X, train_y)
-# best_params = grid_search.best_params_
-# print("Best Hyperparameters:", best_params)
-# best_model2 = grid_search.best_estimator_
+sgd = ['sgd', acc, prec, f1]
 
 
 
-test_y = strat_test_set['median_house_value']
-df_test_X = strat_test_set.drop(columns = ["median_house_value"])
-scaled_data_test = my_scaler.transform(df_test_X.iloc[:,0:-5])
-scaled_data_test_df = pd.DataFrame(scaled_data_test, columns=df_test_X.columns[0:-5])
-test_X = scaled_data_test_df.join(df_test_X.iloc[:,-5:])
-test_X["rooms_per_household"] = test_X["total_rooms"]/test_X["households"]
-test_X["bedrooms_per_room"] = test_X["total_bedrooms"]/test_X["total_rooms"]
-test_X["population_per_household"]=test_X["population"]/test_X["households"]
-test_X = test_X[new_order]
-test_X = test_X.drop(['longitude'], axis=1)
-test_X = test_X.drop(['total_bedrooms'], axis=1)
-test_X = test_X.drop(['population'], axis=1)
-test_X = test_X.drop(['households'], axis=1)
 
-# model1_test_predictions = model1.predict(test_X)
-model2_test_predictions = model2.predict(test_X)
-# model1_test_mae = mean_absolute_error(model1_test_predictions, test_y)
-model2_test_mae = mean_absolute_error(model2_test_predictions, test_y)
-# print("Model 1 MAE is: ", round(model1_test_mae,2))
-print("Model 2 MAE is: ", round(model2_test_mae,2))
+# random forest classifier
 
+forest_clf = RandomForestClassifier(random_state=42)
+forest_clf.fit(df,df_labels)
+forest_pred = forest_clf.predict(df)
+forest_mse = mean_squared_error(df_labels,forest_pred)
+forest_rmse = np.sqrt(forest_mse)
+print('forest')
+print(forest_rmse)
+scores = cross_val_score(forest_clf, df, df_labels,scoring="neg_mean_squared_error", cv=10)
+clf_scores = np.sqrt(-scores)
+print('scores')
+print(scores)
+print(scores.mean())
+print(scores.std())
+
+param_grid = [{'n_estimators': [3, 10, 30, 35], 'max_features': [3, 6, 8, 12]},{'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3,4]},]
+grid_search = GridSearchCV(forest_clf, param_grid, cv=10,scoring='accuracy',return_train_score=True)
+grid_search.fit(df, df_labels)
+print(grid_search.best_params_)
+
+forest_clf=RandomForestClassifier(bootstrap=True, max_depth=None,
+max_features=6, max_leaf_nodes=None, min_impurity_decrease=0.0, min_samples_leaf=1,min_samples_split=2, min_weight_fraction_leaf=0.0,
+n_estimators=3, n_jobs=None, oob_score=False, random_state=42,verbose=0, warm_start=False)
+forest_clf.fit(df,df_labels)
+forest_pred = forest_clf.predict(df)
+forest_mse = mean_squared_error(df_labels,forest_pred)
+forest_rmse = np.sqrt(forest_mse)
+print('forest')
+print(forest_rmse)
+scores = cross_val_score(forest_clf, df, df_labels,scoring="neg_mean_squared_error", cv=10)
+clf_scores = np.sqrt(-scores)
+print('scores')
+print(scores)
+print(scores.mean())
+print(scores.std())
+
+# precsion, confusion matix, accuracy, F1 score
+print('accuracy')
+acc = cross_val_score(forest_clf, df, df_labels, cv=10,scoring="accuracy")
+
+forestpred = cross_val_predict(forest_clf, df, df_labels, cv=10)
+print('confusion matrix')
+
+print(confusion_matrix(df_labels, forestpred))
+
+prec = precision_score(df_labels, forest_pred, average = 'micro')
+print(prec)
+
+f1 = f1_score(df_labels, forestpred, average = 'micro')
+print(f1)
+
+random = ['random', acc, prec, f1]
+
+
+
+# GaussianNB
+gnb_clf = GaussianNB()
+gnb_clf.fit(df,df_labels)
+gnb_pred = gnb_clf.predict(df)
+scores = cross_val_score(gnb_clf, df, df_labels,scoring="neg_mean_squared_error", cv=10)
+clf_scores = np.sqrt(-scores)
+print('scores')
+print(scores)
+print(scores.mean())
+print(scores.std())
+
+
+
+# hyperparamter fine tuning 
+param_nb = {'var_smoothing':np.logspace(0,-5, num=10)}
+
+grid_search = GridSearchCV(gnb_clf, param_nb, cv=10,scoring='accuracy', verbose=1)
+grid_search.fit(df,df_labels)
+print(grid_search.best_params_)
+
+# apply new best hyperparsmters
+gnb_clf = grid_search.fit(df,df_labels)
+gnb_pred = gnb_clf.predict(df)
+scores = cross_val_score(gnb_clf, df, df_labels,scoring="neg_mean_squared_error", cv=10)
+clf_scores = np.sqrt(-scores)
+print('scores')
+print(scores)
+print(scores.mean())
+print(scores.std())
+
+# precsion, confusion matix, accuracy, F1 score
+print('accuracy')
+acc = cross_val_score(gnb_clf, df, df_labels, cv=10,scoring="accuracy")
+
+gnbpred = cross_val_predict(gnb_clf, df, df_labels, cv=10)
+print('confusion matrix')
+
+print(confusion_matrix(df_labels, gnbpred))
+
+prec = precision_score(df_labels, gnb_pred, average = 'micro')
+print(prec)
+
+f1 = f1_score(df_labels, gnbpred, average = 'micro')
+print(f1)
+
+gauss = ['gauss', acc, prec, f1]
+
+compare = [sgd, random, gauss]
 
 
 
