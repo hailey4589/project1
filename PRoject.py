2198f5eff7b8303fd35def94f3f8ef76f5e7bdfe
hailey3456 <119626099+hailey3456@users.noreply.github.com>
2023-10-12 13:47:33 -0400

    almost done
    final joblib left, and final formatting

--- PRoject.py
+++ PRoject.py
@@ -1,42 +1,48 @@
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_absolute_error
+# all stuff needed to be imported for later use 
 import pandas as pd
 # from pandas.plotting import scatter_matrix
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import StratifiedShuffleSplit
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
-from sklearn.metrics import mean_squared_error, f1_score, precision_score, confusion_matrix
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import f1_score, precision_score, confusion_matrix
 from sklearn.model_selection import cross_val_score, cross_val_predict
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
-from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.naive_bayes import GaussianNB
 
+#DATA PROCESSING  
 #import data
 df = pd.read_csv("Project_1_Data.csv")
 
-# print(df.isna().any(axis=0).sum())  #how many columns have missing values?
-# print(df.isna().any(axis=1).sum()) #how many rows have missing values?
+
+# DATA VISULIZATION 
+
+# checking for missing values 
+print(df.isna().any(axis=0).sum())
+print(df.isna().any(axis=1).sum()) 
+# There are no missing values so the next two lines are commented out but\n
+# is how you would fix missing values 
 #df = df.dropna()
 #df = df.reset_index(drop=True)
+
+# Getting basic information about the DataFrame
 
 print("First few rows of the DataFrame:")
 print(df.head())
 
-# Getting basic information about the DataFrame
 print("\nDataFrame info:")
 print(df.info())
 
 print('\nDecribing data')
 print(df.describe())
 
+# Plot histograms for the data see if there are any obvious relations 
 df.hist(bins=50, figsize=(20, 15))
 plt.show()
-print(df.isna().any(axis=0).sum())  # how many columns have missing values?
-print(df.isna().any(axis=1).sum())  # how ma
+
 
 df["attributes"] = pd.cut(
     df["Z"], bins=[0, 1, 2, 3, np.inf], right=False, labels=[0.5, 1.5, 2.5, 3.5])
@@ -75,53 +81,41 @@ print('norm df')
 print(df['X'])
 
 '''
-# SGD classifier
-sgd_clf = SGDClassifier(random_state=42)
-sgd_clf.fit(df,df_labels)
 
-sgd_pred = sgd_clf.predict(df)
-scores = cross_val_score(sgd_clf, df, df_labels,scoring="neg_mean_squared_error", cv=5)
-clf_scores = np.sqrt(-scores)
-print('scores')
-print(scores)
-print(scores.mean())
-print(scores.std())
+# MODEL DEVELOPMENT & ANALYSIS 
+# tree classifier
+tree_clf = DecisionTreeClassifier(random_state=42)
+tree_clf.fit(df,df_labels)
+
+tree_pred = tree_clf.predict(df)
+
 print('accuracy')
-acc = cross_val_score(sgd_clf, df, df_labels, cv=10,scoring="accuracy")
+acc = cross_val_score(tree_clf, df, df_labels, cv=10,scoring="accuracy")
 print(acc)
 
-param_grid = {'loss':['log_loss','modified_huber'],'penalty':['elasticnet'],'alpha':np.logspace(0, 10, 10),'l1_ratio':[0.05,0.06,0.07,0.08,0.09,0.1,0.12,0.13,0.14,0.15,0.2]}
-grid_search = GridSearchCV(sgd_clf, param_grid, cv=5,scoring='accuracy',return_train_score=True)
-sgd_clf = grid_search.fit(df, df_labels)
+param_grid = {'criterion':['gini', 'entropy'],'max_depth' :[2,4,6,8,10,12], 'min_samples_split':[2,4,6,8]}
+grid_search = GridSearchCV(tree_clf, param_grid, cv=5,scoring='accuracy',return_train_score=True)
+tree_clf = grid_search.fit(df, df_labels)
 print(grid_search.best_params_)
-sgd_pred = sgd_clf.predict(df)
-scores = cross_val_score(sgd_clf, df, df_labels,scoring="neg_mean_squared_error", cv=5)
-#clf_scores = np.sqrt(-scores)
-print('scores')
-print(scores)
-print(scores.mean())
-print(scores.std())
-
+tree_pred = tree_clf.predict(df)
 
 # precsion, confusion matix, accuracy, F1 score
 print('accuracy')
-acc = cross_val_score(sgd_clf, df, df_labels, cv=10,scoring="accuracy")
+acc = cross_val_score(tree_clf, df, df_labels, cv=10,scoring="accuracy")
 print(acc)
 
-dfpred = cross_val_predict(sgd_clf, df, df_labels, cv=5)
+dfpred = cross_val_predict(tree_clf, df, df_labels, cv=5)
 #print('confusion matrix')
 
 #print(confusion_matrix(df_labels, dfpred))
 
-prec = precision_score(df_labels, sgd_pred, average = 'micro')
+prec = precision_score(df_labels, tree_pred, average = 'micro')
 print(prec)
 
 f1 = f1_score(df_labels, dfpred, average = 'micro')
 print(f1)
 
-sgd = ['sgd', acc, prec, f1]
-
-
+tree = ['tree', acc, prec, f1]
 
 
 # random forest classifier
@@ -129,37 +123,14 @@ sgd = ['sgd', acc, prec, f1]
 forest_clf = RandomForestClassifier(random_state=42)
 forest_clf.fit(df,df_labels)
 forest_pred = forest_clf.predict(df)
-forest_mse = mean_squared_error(df_labels,forest_pred)
-forest_rmse = np.sqrt(forest_mse)
-print('forest')
-print(forest_rmse)
-scores = cross_val_score(forest_clf, df, df_labels,scoring="neg_mean_squared_error", cv=10)
-clf_scores = np.sqrt(-scores)
-print('scores')
-print(scores)
-print(scores.mean())
-print(scores.std())
+
 
 param_grid = [{'n_estimators': [3, 10, 30, 35], 'max_features': [3, 6, 8, 12]},{'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3,4]},]
 grid_search = GridSearchCV(forest_clf, param_grid, cv=10,scoring='accuracy',return_train_score=True)
-grid_search.fit(df, df_labels)
+forest_clf = grid_search.fit(df, df_labels)
 print(grid_search.best_params_)
 
-forest_clf=RandomForestClassifier(bootstrap=True, max_depth=None,
-max_features=6, max_leaf_nodes=None, min_impurity_decrease=0.0, min_samples_leaf=1,min_samples_split=2, min_weight_fraction_leaf=0.0,
-n_estimators=3, n_jobs=None, oob_score=False, random_state=42,verbose=0, warm_start=False)
-forest_clf.fit(df,df_labels)
 forest_pred = forest_clf.predict(df)
-forest_mse = mean_squared_error(df_labels,forest_pred)
-forest_rmse = np.sqrt(forest_mse)
-print('forest')
-print(forest_rmse)
-scores = cross_val_score(forest_clf, df, df_labels,scoring="neg_mean_squared_error", cv=10)
-clf_scores = np.sqrt(-scores)
-print('scores')
-print(scores)
-print(scores.mean())
-print(scores.std())
 
 # precsion, confusion matix, accuracy, F1 score
 print('accuracy')
@@ -184,13 +155,6 @@ random = ['random', acc, prec, f1]
 gnb_clf = GaussianNB()
 gnb_clf.fit(df,df_labels)
 gnb_pred = gnb_clf.predict(df)
-scores = cross_val_score(gnb_clf, df, df_labels,scoring="neg_mean_squared_error", cv=10)
-clf_scores = np.sqrt(-scores)
-print('scores')
-print(scores)
-print(scores.mean())
-print(scores.std())
-
 
 
 # hyperparamter fine tuning 
@@ -215,9 +179,11 @@ print('accuracy')
 acc = cross_val_score(gnb_clf, df, df_labels, cv=10,scoring="accuracy")
 
 gnbpred = cross_val_predict(gnb_clf, df, df_labels, cv=10)
+print(gnbpred)
 print('confusion matrix')
-
-print(confusion_matrix(df_labels, gnbpred))
+cm = confusion_matrix(df_labels, gnbpred)
+print(cm)
+sns.heatmap(np.abs(cm))
 
 prec = precision_score(df_labels, gnb_pred, average = 'micro')
 print(prec)
@@ -227,7 +193,7 @@ print(f1)
 
 gauss = ['gauss', acc, prec, f1]
 
-compare = [sgd, random, gauss]
+compare = [tree, random, gauss]
 
 
 
